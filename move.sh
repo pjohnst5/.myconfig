a1d5812fc999e6a60d5d8095175e5de5aa929ef5
Paul Johnston <johnstonpaul801@gmail.com>
2020-09-17 20:16:29 -0700

    Added it so files are moved from directory of script/files

--- move.sh
+++ move.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# get directory of this script
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 # copy files to home
-cp ./files/.[a-zA-Z0-9]* ~/
+cp "$DIR"/files/.[a-zA-Z0-9]* ~/
 
 # source bash profile
 source ~/.profile
