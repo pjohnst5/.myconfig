551a5ba0f57c1009a1068c0901f06904a0cf3545
Paul Johnston <johnstonpaul801@gmail.com>
2020-09-29 11:49:33 -0700

    kubectl

--- move.sh
+++ move.sh
@@ -6,6 +6,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # copy from files to home
 cp "$DIR"/files/.[a-zA-Z0-9]* ~/
 
+# setup kubectl alias
+alias k=kubectl
+
 # Instll Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
